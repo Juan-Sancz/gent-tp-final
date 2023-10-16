cdd9010e6ca468d5d799a083d488901947a1a36f
Juan-Sancz <131791245+Juan-Sancz@users.noreply.github.com>
2023-10-16 03:07:59 -0300

    sistema

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+
+import React, { useEffect, useState } from 'react';
+import Shop from './components/shop.js'; // Import the InsertForm component
+import Signup from './components/signup.js';
+import Login from './components/login.js';
+import Regispet from './components/regispet.js';
+import Adop from './components/adop.js';
+import Prod from './components/prod.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/home' element={<Shop />}></Route>
+        <Route path='/' element={<Shop />}></Route>
+        <Route path='/regispet' element={<Regispet />}></Route>
+        <Route path='/adop' element={<Adop />}></Route>
+        <Route path='/prod' element={<Prod />}></Route>
+
+      </Routes>
+    
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
